72186f2a2b17ca9dd41f82bf05718bc7f765495b
Adimchi <oraegbunamadimchi@gmail.com>
2022-09-23 20:38:55 +0100

    added functionality

---
+++ src/app.js
@@ -0,0 +1,25 @@
+let password = document.querySelector('#password');
+let generate = document.querySelector('#generate');
+let copyButton = document.querySelector('#copytext');
+function generatePass(e) {
+    e.preventDefault();
+    
+    let characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890~`!@#$%^&*()_-+={[}]|\:;"<,>.?/';
+    let passwordLength = 12
+    let password = ''
+    for (let i = 0; i <= passwordLength; i++) {
+       let getRandNum = Math.floor(Math.random() * characters.length)
+       password += characters.substring(getRandNum, getRandNum + 1)
+        
+    }
+    document.querySelector('#password').value = password
+}
+function copy(e) {
+    e.preventDefault();
+    let copyPassword = document.querySelector('#password')
+    copyPassword.select()
+    copyPassword.setSelectionRange(0, 999)
+    document.execCommand('copy')
+}
+generate.addEventListener('click', generatePass)
+copyButton.addEventListener('click', copy)
